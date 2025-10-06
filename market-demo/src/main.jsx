23fc78d660975a28d40341a73cd5c946da17a491
Lamin Mbye-Prior <astrosilicon@gmail.com>
2025-10-06 03:52:03 +0100

    added market demo

--- market-demo/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import LumeraMarketApp from './LumeraMarketApp.jsx'
-
-createRoot(document.getElementById('root')).render(<LumeraMarketApp />)
